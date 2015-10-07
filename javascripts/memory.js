2014d9aaf535cdea1f690289761480647df4d34c
SchoolYW <kahuligi@postonline.me>
2015-10-07 20:21:39 +0200

    Update memory.js
    create cards dynamically and shuffle numbers

--- javascripts/memory.js
+++ javascripts/memory.js
@@ -1,16 +1,82 @@
 function memoryObject(){
-    this.cardSelected = -1;
-    this.cardIds = [0, 1, 2, 3, 4, 5, 6, 7];
+	this.cardAmount = 0;
+	this.cardCounter = 0;
+	this.cardSelected = -1;
     this.isAvailable = true;
-    
-    this.changeVisibility = function(element){
-        var span = element.getElementsByTagName("span")[0];
-        if(span.style.visibility=="visible"){
-            span.style.visibility = "hidden";
-        }else{
-            span.style.visibility = "visible";
-        }
-    }
+	
+	this.createCard = function(){
+		var card = document.createElement("div");
+		
+		card.id = this.cardCounter++;
+		card.setAttribute("class", "card");
+		card.setAttribute("onClick", "memory.cardChosen(this)");
+		card.setAttribute("data-finalized", "false");
+		
+		var numberPanel = document.createElement("span");
+		numberPanel.setAttribute("class", "numberPanel");
+		card.appendChild(numberPanel);
+		
+		return card;
+	}
+	
+	this.createCardRow = function(columns){
+		var cardRow = document.createElement("div");
+		cardRow.setAttribute("class", "row"); 
+		
+		for (var i = 0; i < columns; i++)
+		{
+			var card = this.createCard();
+			cardRow.appendChild(card);
+		}
+		
+		return cardRow;
+	}
+	
+	function shuffle(o)
+	{
+		for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+		return o;
+	}
+	
+	this.shuffleCards = function(){
+		var numbers = [];
+		for (var i = 1; i <= (this.cardAmount / 2); i++)
+		{
+			numbers.push(i);
+			numbers.push(i);
+		}
+		
+		var shuffledNumbers = shuffle(numbers);
+		for (var i = 0; i < this.cardAmount; i++)
+		{
+			var card = document.getElementById(i);
+			var span = card.getElementsByTagName("span")[0];
+			span.innerHTML = shuffledNumbers[i];
+		}
+	}
+	
+	this.createPlayfield = function(amount){
+		this.cardAmount = amount * 2;
+		var playfield = document.getElementById("playfield");
+		
+		var rows = Math.sqrt(this.cardAmount);
+		var columns = Math.ceil(rows);
+		for (var i = 0; i<Math.floor(rows); i++)
+		{
+			var cardRow = this.createCardRow(columns);
+			playfield.appendChild(cardRow);
+		}
+		
+		var residue = this.cardAmount % columns;
+		if (residue != 0)
+		{
+			var cardRow = this.createCardRow(residue);
+			playfield.appendChild(cardRow);
+		}
+		
+		this.shuffleCards();
+	}
+	
     this.cardChosen = function(element){
         if(element.getAttribute("data-finalized") == "false" && this.cardSelected != element.id && this.isAvailable){
             this.changeVisibility(element);
@@ -25,8 +91,6 @@ function memoryObject(){
                     this.isAvailable = false;
                     var secondCard = document.getElementById(this.cardSelected);
                     window.setTimeout(this.resetCards,500, this, element, secondCard);
-                    //this.changeVisibility(element);
-                    //this.changeVisibility(document.getElementById(this.cardSelected));
                 }
                 this.cardSelected = -1;
             }else{
@@ -34,6 +98,15 @@ function memoryObject(){
             }
         }
     }
+	
+	this.changeVisibility = function(element){
+		var span = element.getElementsByTagName("span")[0];
+		if(span.style.visibility=="visible"){
+			span.style.visibility = "hidden";
+		}else{
+			span.style.visibility = "visible";
+	}
+}
     
     this.resetCards = function(object, firstCard, secondCard)
     {
@@ -56,10 +129,14 @@ function memoryObject(){
 var memory = new memoryObject();
 var timerCount = 0;
 var failCount = 0;
-window.setTimeout(timerFunction, 1000);
-function timerFunction()
-{
-    document.getElementById("timer").innerHTML = timerCount++;
-    window.setTimeout(timerFunction, 1000);
-}
 
+window.onload = function(){
+	window.setTimeout(timerFunction, 1000);
+	memory.createPlayfield(4);
+
+	function timerFunction()
+	{
+		document.getElementById("timer").innerHTML = timerCount++;
+		window.setTimeout(timerFunction, 1000);
+	}
+}
