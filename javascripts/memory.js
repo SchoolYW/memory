8a24c2dc74be1f68c75370db4a5b268465949c43
SchoolYW <kahuligi@postonline.me>
2015-10-07 21:55:08 +0200

    Update memory.js
    change timer layout

--- javascripts/memory.js
+++ javascripts/memory.js
@@ -128,15 +128,30 @@ function memoryObject(){
 
 var memory = new memoryObject();
 var timerCount = 0;
-var failCount = 0;
+var failCount = 1;
 
-window.onload = function(){
+function toTimer(seconds)
+{
+	var sec_num = parseInt(seconds, 10);
+	var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+	
+	if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
+function timerFunction()
+{
+	document.getElementById("timer").innerHTML = toTimer(timerCount);
+	timerCount++
 	window.setTimeout(timerFunction, 1000);
+}
+	
+window.onload = function(){
 	memory.createPlayfield(4);
-
-	function timerFunction()
-	{
-		document.getElementById("timer").innerHTML = timerCount++;
-		window.setTimeout(timerFunction, 1000);
-	}
+	timerFunction();
 }
