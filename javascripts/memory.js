85c1a1c92a09bbca0b41e1302afc68fcf842a1b9
btschool123 <bennitank@web.de>
2015-10-07 13:55:27 +0200

    Update memory.js
    added time control for card changes

--- javascripts/memory.js
+++ javascripts/memory.js
@@ -1,6 +1,7 @@
 function memoryObject(){
     this.cardSelected = -1;
     this.cardIds = [0, 1, 2, 3, 4, 5, 6, 7];
+    this.isAvailable = true;
     
     this.changeVisibility = function(element){
         var span = element.getElementsByTagName("span")[0];
@@ -20,14 +21,23 @@ function memoryObject(){
                     document.getElementById(this.cardSelected).setAttribute("data-finalized", true);
                     element.setAttribute("data-finalized", true);
                 }else{
-                    this.changeVisibility(element);
-                    this.changeVisibility(document.getElementById(this.cardSelected));
+                    this.isAvailable = false;
+                    this.setTimeout(restCards(element, document.getElementById(this.cardSelected)),500);
+                    //this.changeVisibility(element);
+                    //this.changeVisibility(document.getElementById(this.cardSelected));
                 }
                 this.cardSelected = -1;
             }else{
                 this.cardSelected = element.id;
             }
         }
+    }
+    
+    this.resetCards = function(firstCard, secondCard)
+    {
+        this.changeVisibility(firstCard);
+        this.changeVisibility(secondCard);
+        this.isAvailable = true;
     }
 }
 
