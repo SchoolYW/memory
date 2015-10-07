101ca13c49444208cffa67631043aca5828d3e18
SchoolYW <kahuligi@postonline.me>
2015-10-07 08:40:04 +0200

    Update memory.js
    mehr Funktionalität

--- javascripts/memory.js
+++ javascripts/memory.js
@@ -1,8 +1,32 @@
-function changeVisibility(element){
-  var span = element.getElementsByTagName("span")[0];
-  if(span.style.visibility=="visible"){
-    span.style.visibility = "hidden";
-  }else{
-    span.style.visibility = "visible";
-  }
+function memory(){
+    this.cardSelected = -1;
+    this.cardIds = [0, 1, 2, 3, 4, 5, 6, 7];
+    
+    this.changeVisbility = function(element){
+        var span = element.getElementsByTagName("span")[0];
+        if(span.style.visibility=="visible"){
+            span.style.visibility = "hidden";
+        }else{
+            span.style.visibility = "visible";
+        }
+    }
+    this.cardChosen = function(element){
+        if(element.getAttribute("data-finalized") != "false"){
+            this.changeVisibility(element);
+            if(this.cardSelected != -1){
+                var selectedValue = document.getElementById(this.cardSelected).getElementsByTagName("span")[0].innerHTML;
+                var currentValue = element.getElementsByTagName("span")[0].innerHTML;
+                if(selectedValue == currentValue){
+                    document.getElementById(this.cardSelected).setAttribute("data-finalized", true);
+                    element.setAttribute("data-finalized", true);
+                }else{
+                    this.changeVisbility(element);
+                    this.changeVisbility(document.getElementById(this.cardSelected));
+                }
+                this.cardSelected = -1;
+            }else{
+                this.cardSelected = element.id;
+            }
+        }
+    }
 }
